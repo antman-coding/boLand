335469c591bd9402a85a94315b85fb2d2ebb8c37
bothebanana <acm25.cool@gmail.com>
2025-02-28 14:43:44 -0700

    revert revert revert revert!

--- scripts/np.js
+++ scripts/np.js
@@ -1,6 +1,5 @@
 function updateNowPlaying() {
-    document.getElementById('nowPlaying').textContent = "Loading... | "
-    setTimeout(function(){
+    
     fetch('/app')
       .then(response => {
         if (!response.ok) {
@@ -17,7 +16,7 @@ function updateNowPlaying() {
         console.error('Error fetching now playing data:', error);
         document.getElementById('nowPlaying').textContent = 'Could not load now playing information ';
       });
-    }, 100)
+    
   }
   
   // Call the function to update immediately
