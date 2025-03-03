dc78f8ba9d1002a397f3f4c49c0295615e185607
bothebanana <acm25.cool@gmail.com>
2025-03-02 22:15:44 -0700

    calmed it down a bit

--- scripts/np.js
+++ scripts/np.js
@@ -1,29 +1,3 @@
-// function updateNowPlaying() {
-    
-//     fetch('/app')
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return response.text();
-//       })
-//       .then(data => {
-//         // Decode URI encoded characters
-//         const decodedData = decodeURIComponent(data);
-//         document.getElementById('nowPlaying').textContent = "BoTheBanana is listening to " + decodeURIComponent(decodedData)+" | ";
-//       })
-//       .catch(error => {
-//         console.error('Error fetching now playing data:', error);
-//         document.getElementById('nowPlaying').textContent = 'Could not load now playing information ';
-//       });
-    
-//   }
-  
-//   // Call the function to update immediately
-//   updateNowPlaying();
-  
-//   // Optionally, set interval to update periodically (every how ever many the fuck i want seconds)
-//   setInterval(updateNowPlaying, 5000);
 
 function updateNowPlaying() {
   fetch('/app')
@@ -46,4 +20,4 @@ function updateNowPlaying() {
 updateNowPlaying();
 
 // got a lot more ballsy about this. surely my server wont get ip banned by librefm Clueless
-setInterval(updateNowPlaying, 1000);
+setInterval(updateNowPlaying, 2000);
